028ba0a5f6d2511061e4925ca68c0c93cc7ca834
Gomolemo <tshabalalagomolemo@gmail.com>
2024-05-29 07:55:18 +0200

    end points

--- src/main/java/com/GroupD/FurnishUp/controller/FurnishUpController.java
+++ src/main/java/com/GroupD/FurnishUp/controller/FurnishUpController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/FurnishUp2")
+@RequestMapping("api/FurnishUp/customers")
 public class FurnishUpController {
 
     @Autowired
