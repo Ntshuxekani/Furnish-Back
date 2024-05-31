635e4a1bbb97345c8359a4c7413312272607fa3a
mpho2024 <mpho.tsatsi01@gmail.com>
2024-05-31 08:48:37 +0200

    joooh

--- src/main/java/com/GroupD/FurnishUp/controller/ProductsController.java
+++ src/main/java/com/GroupD/FurnishUp/controller/ProductsController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+//@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("api/v1/FurnishUp")
+@RequestMapping("api/FurnishUp/products")
 public class ProductsController {
 
     @Autowired
